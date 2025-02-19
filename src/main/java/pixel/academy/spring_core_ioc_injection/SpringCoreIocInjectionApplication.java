6d2582afae18db45e25dfe7e451b5e67fc7143d6
Sorin Gore <soringoredev@gmail.com>
2025-02-19 13:23:52 +0200

    Initial commit.

---
+++ src/main/java/pixel/academy/spring_core_ioc_injection/SpringCoreIocInjectionApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.spring_core_ioc_injection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreIocInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreIocInjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academy/spring_core_ioc_injection/SpringCoreIocInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.spring_core_ioc_injection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreIocInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
